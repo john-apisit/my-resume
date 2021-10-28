b317abe940cce5ff93c3cb329c7f51f578f4a54e
Apisit Sianglert <apisit.sianglert2@tesco.com>
2021-10-28 21:33:27 +0700

    SmileCheckBox

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from "vue-router";
 import Profile from "../views/Profile.vue";
+import SmileCheckBox from "../views/SmileCheckBox.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Profile",
     component: Profile,
+  },
+  {
+    path: "/SmileCheckBox",
+    name: "SmileCheckBox",
+    component: SmileCheckBox,
   },
 ];
 
---
+++ src/views/SmileCheckBox.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="page">
+    <label class="checkbox-form">
+      <input type="checkbox" />
+      <span><i></i></span>
+    </label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+
+  label.checkbox-form {
+    position: relative;
+    width: 130px;
+    height: 48px;
+
+    input[type='checkbox'] {
+      position: absolute;
+      appearance: none;
+
+      &:checked ~ span {
+        background-color: #00d900;
+        box-shadow: 0 15px 25px #009700a6;
+
+        i {
+          left: 84px;
+
+          &::before {
+            background-color: #00d900;
+            box-shadow: 20px 0 0 #00d900;
+          }
+
+          &::after {
+            background-color: #00d900;
+            height: 10px;
+            border-radius: 0 0 50px 50px;
+            top: 24px;
+          }
+        }
+      }
+    }
+
+    span {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: red;
+      box-shadow: 0 15px 25px #d80000a6;
+      border-radius: 80px;
+      transition: 0.5s;
+
+      i {
+        position: absolute;
+        background-color: white;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        top: 4px;
+        left: 4px;
+        transition: 0.5s;
+
+        &::before {
+          content: '';
+          position: absolute;
+          background-color: red;
+          width: 8px;
+          height: 8px;
+          border-radius: 50%;
+          top: 12px;
+          left: 6px;
+          box-shadow: 20px 0 0 red;
+          transition: 0.5s;
+          
+        }
+
+        &::after {
+          content: '';
+          position: absolute;
+          background-color: red;
+          width: 18px;
+          height: 6px;
+          border-radius: 80px;
+          top: 26px;
+          left: 11px;
+          transition: 0.5s;
+        }
+      }
+    }
+  }
+}
+</style>
